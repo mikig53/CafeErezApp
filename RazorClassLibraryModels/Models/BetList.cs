2a5610f9ca8d5fa4d69e588130a861d3922996eb
mikig53 <30700064+mikig53@users.noreply.github.com>
2024-03-09 15:18:43 +0200

    Add files via upload
    adding RazorClassLibraryModels project

---
+++ RazorClassLibraryModels/Models/BetList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibraryModels.Models;
+public class BetList
+{
+    public string GameNo { get; set; }
+    public decimal BetRatio { get; set; }
+    public string Bet1X2 { get; set; }
+    public string Game { get; set; }
+    public string Game2 { get; set; }
+    public string Type { get; set; }
+    public int ClickedButton { get; set; }
+    public string Info { get; set; }
+}
